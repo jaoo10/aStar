d6d1fb5f02e335e79df887ed310cbc7a6b334fdc
John <john@john-programmer.(none)>
2013-09-29 14:54:23 -0300

    Agora ta brutal as heurísticas e o algoritmo.

--- NumberPuzzle.py
+++ NumberPuzzle.py
@@ -6,9 +6,14 @@ import math
 import AStar
 
 class NumberPuzzle(AStar.AStar):
-    def __init__(self,heuristic):
-        self.heuristic = heuristic
+    #Choose which heuristic to use, weight1 is the weight of the 1st heuristic
+    #and so on.
+    def __init__(self,weight1,weight2,weight3):
+        self.weight1 = weight1
+        self.weight2 = weight2
+        self.weight3 = weight3
 
+    #Manhattan distance of the empty square
     def distBetween(self,current,neighbor):
         coord = []
         for i in range(len(current)):
@@ -16,23 +21,25 @@ class NumberPuzzle(AStar.AStar):
                 if current[i][j] == 0 or neighbor[i][j] == 0:
                     coord.append([i,j])
                 if len(coord) == 2:
-                    firstPosition = pow(coord[0][0] - coord[1][0],2)
-                    secondPosition = pow(coord[0][1] - coord[1][1],2)
-                    return math.sqrt(firstPosition + secondPosition)
+                    firstPosition = abs(coord[0][0] - coord[1][0])
+                    secondPosition = abs(coord[0][1] - coord[1][1])
+                    return firstPosition + secondPosition
         return 0
 
     def firstHeuristic(self,start,goal,i,j):
         if start[i][j] != goal[i][j]:
-            self.cost += 1
+            self.cost1 += 1
 
     def secondHeuristic(self,start,goal,i,j):
         if j + 1 < len(start[i]):
             if start[i][j] != start[i][j+1] - 1:
-                self.cost += 1
+                self.cost2 += 1
         elif i + 1 < len(start):
             if start[i][j] != start[i+1][0] - 1:
-                self.cost += 1
+                self.cost2 += 1
 
+    #Manhattan distance + Euclidian distance 
+    #(the fastest, but not the best solution)
     def thirdHeuristic(self,start,goal,i,j):
         if start[i][j] != goal[i][j]:
             correctI = None
@@ -43,19 +50,22 @@ class NumberPuzzle(AStar.AStar):
             else:
                 correctI = (start[i][j] - 1) / len(start)
                 correctJ = (start[i][j] - 1) % len(start[i])
-            self.cost += math.sqrt(pow(correctI - i,2) + pow(correctJ - j,2))
+            self.cost3 += abs(correctI - i) + abs(correctJ - j)
+            #self.cost3 += math.sqrt(pow(correctI - i,2) + pow(correctJ - j,2))
 
     def heuristicEstimate(self,start,goal):
-        self.cost = 0
+        self.cost1 = 0
+        self.cost2 = 0
+        self.cost3 = 0
         for i in range(len(start)):
             for j in range(len(start[i])):
-                if self.heuristic & 0b1:
                     self.firstHeuristic(start,goal,i,j)
-                if self.heuristic & 0b10:
                     self.secondHeuristic(start,goal,i,j)
-                if self.heuristic & 0b100:
                     self.thirdHeuristic(start,goal,i,j)
-        return self.cost
+        self.cost1 *= self.weight1
+        self.cost2 *= self.weight2
+        self.cost3 *= self.weight3
+        return self.cost1 + self.cost2 + self.cost3
 
     def neighborNodes(self,current):
         for i in range(len(current)):
@@ -89,42 +99,30 @@ class NumberPuzzle(AStar.AStar):
             print
         print "%d Movements" % (len(path) - 1)
 
+def getPuzzle(filename):
+    f = open(filename)
+    puzzle = []
+    for line in f:
+        puzzle.append(tuple(map(int,line.split())))
+    f.close()
+    return tuple(puzzle)
+
 if __name__ == "__main__":
     finalState = ((1,2,3,4),(5,6,7,8),(9,10,11,12),(13,14,15,0))
-    
-    examples = {
-            1: ((1,6,2,3),(5,10,7,4),(9,14,11,8),(13,0,15,12)),
-            2: ((2,0,3,4),(1,6,7,8),(5,9,10,11),(13,14,15,12)),
-            3: ((2,6,8,3),(1,14,9,11),(7,12,13,0),(5,15,4,10)),
-            4: ((1,6,2,3),(5,10,7,0),(4,9,14,11),(8,13,15,12)),
-            5: ((1,6,2,3),(5,10,0,7),(4,9,14,11),(8,13,15,12)),
-            6: ((1,6,2,3),(5,0,10,7),(4,9,14,11),(8,13,15,12)),
-            7: ((1,6,2,3),(0,5,10,7),(4,9,14,11),(8,13,15,12)),
-            }
 
-    if len(sys.argv) > 2:
-        ex = int(sys.argv[1])
-        heuristic = None
-        
+    if len(sys.argv) > 4:
         try:
-            heuristic = int(sys.argv[2],2)
-        except:
-            print "Insert a binary number for the second arg."
-            sys.exit(1)
+            example = getPuzzle(sys.argv[1])
+            weight1 = int(sys.argv[2])
+            weight2 = int(sys.argv[3])
+            weight3 = int(sys.argv[4])
+            puzzle = NumberPuzzle(weight1,weight2,weight3)
+            path = puzzle.aStar(example,finalState)
+            puzzle.printPath(path)
         
-        if ex in examples:
-            try:
-                puzzle = NumberPuzzle(heuristic)
-                path = puzzle.aStar(examples[ex],finalState)
-                puzzle.printPath(path)
-            except:
-                print "Program interrupted."
-                sys.exit(1)
-        else:
-            print "Invalid arg."
+        except BaseException as error:
+            print(error)
+            sys.exit(1)
     
     else:
-        print "Insert two args."
-        for keys in examples:
-            print keys,
-        print "\nAnd a binary number."
+        print "Insert four args."
